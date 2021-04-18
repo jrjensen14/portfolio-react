1147c4616716fd3319b4423103af8d1d9ae595a4
JRJ <jrjensen14@gmail.com>
2021-04-18 14:53:54 -0500

    portfolio

---
+++ src/components/Project/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { removeHyphensAndCapitalize } from '../../utils';
+
+function Project({ project }) {
+
+  const { name, repo, link,  description } = project;
+  
+  return (
+    <div className="project" key={name}>
+      <img
+        src={require(`../../assets/projects/${name}.jpg`)}
+        alt={(name)}
+        className="project-bg"
+      />
+      <div className="project-text">
+        <h3>
+          <a href={link}>{(name)}</a>{' '}
+          <a href={repo}>
+            <i className="fab fa-github"></i>
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
